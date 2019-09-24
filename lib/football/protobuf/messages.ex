50b1531299f616ee28a266d6624db9d7814ab916
auklejewski <artur.uklejewski@gmail.com>
2019-09-24 20:02:30 +0200

    Add proto definitions as submodule

--- lib/football/protobuf/messages.ex
+++ lib/football/protobuf/messages.ex
@@ -1,3 +1,3 @@
 defmodule Football.Protobuf.Messages do
-  use Protobuf, from: Path.expand("../../../priv/proto/messages.proto", __DIR__)
+  use Protobuf, from: Path.expand("../../../priv/proto-definitions/messages.proto", __DIR__)
 end
